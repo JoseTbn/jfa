e7d23a18a1b05786424b46c858dfa74ed17408da
JoseTbn <josebatumeni@gmail.com>
2022-07-05 20:50:50 +0200

    supp node-sass by sass = errors with compatiblity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,22 +15,22 @@ className="lg:inline-block lg:text-xl lg:ml-14 lg:pb-0.5 lg:  lg:no-underline md
 
   <ul className='flex flex-row '>
     <Link to={'/Work'}><a class='group  lg:text-xl flex flex-col   lg:relative lg:overflow-hidden  transition-all duration-500 ease-in-out hover:bg-orange-700 lg:no-underline md:text-xs font-semibold uppercase text-gray-600 tracking-wider border border-gray-900   rounded-full  w-32  h-full '>
-      <span  className="absolute  justify-center ml-6 transition-transform duration-300 ease-in-out group-hover:translate-y-[-50px] mx-auto w-full px-auto " >work</span>
+      <span  className="absolute  justify-center ml-8 transition-transform duration-300 ease-in-out group-hover:translate-y-[-50px] mx-auto w-full px-auto " >work</span>
       <span className="absolute 
-      justify-center ml-6 mx-auto w-full px-auto
+      justify-center ml-8 mx-auto w-full px-auto
       transition-transform duration-300 ease-in-out  translate-y-[50px] group-hover:-translate-y-0 " >work</span>
     </a>
     </Link >
    
     <Link to={'/About'}><a class=' group flex flex-col   lg:text-xl lg:ml-14 lg:pb-0.5 lg:relative lg:overflow-hidden  transition-all duration-500 ease-in-out hover:bg-orange-700 lg:no-underline md:text-xs font-semibold uppercase text-gray-600 tracking-wider border border-gray-900 p-2  rounded-full  w-32  h-full' >
-      <span  className="absolute ml-5  justify-center mx-auto w-full px-auto  transition-transform duration-300 ease-in-out group-hover:translate-y-[-50px]">about</span>
-      <span className="absolute ml-5  justify-centermx-auto w-full px-autotransition-transform duration-300 ease-in-out  translate-y-[50px] group-hover:-translate-y-0" >about</span>
+      <span  className="absolute ml-5 -mt-2  justify-center mx-auto w-full px-auto  transition-transform duration-300 ease-in-out group-hover:translate-y-[-50px]">about</span>
+      <span className="absolute ml-5 -mt-2  justify-centermx-auto w-full px-autotransition-transform duration-300 ease-in-out  translate-y-[50px] group-hover:-translate-y-0" >about</span>
       </a>
       </Link>
     
     <Link to={'/contact'}><a class=' group flex flex-col  lg:text-xl lg:ml-14 lg:pb-0.5 lg:relative lg:overflow-hidden  transition-all duration-500 ease-in-out hover:bg-orange-700 lg:no-underline md:text-xs font-semibold uppercase text-gray-600 tracking-wider border border-gray-900 p-2  rounded-full  w-32  h-full ' >
-      <span  className="absolute ml-2  justify-center mx-auto w-full px-auto   transition-transform duration-300 ease-in-out group-hover:translate-y-[-50px]" >contact</span>
-      <span className="absolute ml-2  justify-center mx-auto w-full px-auto transition-transform duration-300 ease-in-out  translate-y-[50px] group-hover:-translate-y-0" >contact</span>
+      <span  className="absolute ml-2 -mt-2 justify-center mx-auto w-full px-auto   transition-transform duration-300 ease-in-out group-hover:translate-y-[-50px]" >contact</span>
+      <span className="absolute ml-2 -mt-2 justify-center mx-auto w-full px-auto transition-transform duration-300 ease-in-out  translate-y-[50px] group-hover:-translate-y-0" >contact</span>
       </a>
       </Link>
   </ul>
